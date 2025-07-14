07c936c129ec405a83dfdc330b2bed8458236463
mohammadrizvy <rizvyhq1@gmail.com>
2025-07-14 09:06:28 +0600

    Implement code changes to enhance functionality and improve performance

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -1,23 +1,16 @@
-import {  useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-
 const useUsers = () => {
+  const { data, refetch, isLoading, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/users`);
+      return res.data;
+    },
+  });
 
-    const {data , refetch , isLoading , error} = useQuery({
-        queryKey : ["users"],
-        queryFn : async () => {
-
-            const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/users`);
-            return res.data
-
-        }
-        
-    })
-    
-
-
-    return { data, refetch, isLoading, error };
+  return { data, refetch, isLoading, error };
 };
 
 export default useUsers;
